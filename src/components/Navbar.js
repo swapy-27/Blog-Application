7e5ab77ebf8a4778220045aeb5eaf42d22f81fba
Swapnil <swapniljain240@gmail.com>
2022-04-13 14:16:43 +0530

    implementing NavBAr

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
-const Navbar = ()=>{
+import { Link } from 'react-router-dom';
+import '../css/navbar.css'
+const Navbar = () => {
     return (
         <div className="navbar_container">
-            <h2>Navbar</h2>
+            <ul>
+                <li>
+                    <Link className='links' to={'/'} >Home</Link>
+                </li>
+                <li>
+                    <Link className='links' to={'/create-post'} >Create Post</Link>
+                </li>
+                
+            </ul>
         </div>
     )
 }
