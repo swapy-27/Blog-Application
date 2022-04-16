df6a3bb8305b7bd3fa57be8cbb94236b520b6d71
Swapnil <swapniljain240@gmail.com>
2022-04-17 00:08:10 +0530

    fetching data from firestore collection

--- src/components/CreatePostComponent.js
+++ src/components/CreatePostComponent.js
@@ -1,53 +1,53 @@
 import '../css/createpost.css';
-import {useState} from 'react'
-import { doc,setDoc } from 'firebase/firestore/lite';
+
+import { doc, setDoc } from 'firebase/firestore/lite';
 import useFormInput from './hooks';
 
 import db from '../firebase'
 
-const CreatePostComponent =   ()=>{
-    const title= useFormInput('');
-    const subTitle= useFormInput('');
-    const content= useFormInput('');
-    const handleSubmit = async(e)=>{
-            e.preventDefault();
-            
-           const postRef =  doc(db,'posts',title.value);
-           
-           await setDoc(postRef,
+const CreatePostComponent = () => {
+    const title = useFormInput('');
+    const subTitle = useFormInput('');
+    const content = useFormInput('');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const postRef = doc(db, 'posts', title.value);
+
+        await setDoc(postRef,
             {
-                title:title.value,
-                subTitle:subTitle.value,
-                content:content.value,
-                createdAt:new Date()
+                title: title.value,
+                subTitle: subTitle.value,
+                content: content.value,
+                createdAt: new Date()
             })
 
- 
-            
+
+
     }
 
     return (
         <div className="createPost_container">
             <h1>Create Post</h1>
             <form>
-            <div className='form-field'>
-                <label>Title</label>
-                <input {...title}
-                />
+                <div className='form-field'>
+                    <label>Title</label>
+                    <input {...title}
+                    />
 
-            </div>
-            <div className='form-field'>
-                <label>Sub-Title</label>
-                <input {...subTitle}/>
-                
-            </div>
-            <div className='form-field'>
-                <label>Content</label>
-                <textarea {...content}></textarea>
-                
-            </div>
+                </div>
+                <div className='form-field'>
+                    <label>Sub-Title</label>
+                    <input {...subTitle} />
 
-            <button className='create-post-btn' onClick={handleSubmit}>Create-Post</button>
+                </div>
+                <div className='form-field'>
+                    <label>Content</label>
+                    <textarea {...content}></textarea>
+
+                </div>
+
+                <button className='create-post-btn' onClick={handleSubmit}>Create-Post</button>
             </form>
         </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,34 @@
-const Home = ()=>{
+import { useState, useEffect } from 'react';
+import db from '../firebase';
+import { collection } from 'firebase/firestore/lite';
+import { doc, getDoc } from 'firebase/firestore/lite';
+import { getDocs } from 'firebase/firestore/lite';
+const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+
+        getDocs(collection(db, 'posts'))
+            .then(
+                (snapshot) => {
+                    const posts = snapshot.docs.map((doc) => {
+                        return (
+                            {
+                                id: doc.id,
+                                ...doc.data()
+                            }
+                        )
+                    })
+
+                    console.log(posts)
+
+                    setPosts(posts)
+                }
+            );
+
+
+
+    }, [])
     return (
         <div className="home_container">
             <h2>Home</h2>
